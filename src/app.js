5706acae4f986a2224bfeaee4f82257bea91beaf
Azk <azk.akhtar@gmail.com>
2025-10-02 00:49:24 +0530

    routing and middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
+
+app.use("/hello", (req, res, next) => {
+    next();
+}, (req, res) => {
+    res.send("middleware's hello")
+})
 app.listen(3000, ()=> {
     console.log("running");
 });
